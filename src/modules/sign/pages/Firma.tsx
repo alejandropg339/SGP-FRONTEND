5976518d9353b3cff0728e869e306b9e17cf921f
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2023-08-13 17:35:53 -0500

    Minor message update

--- src/modules/sign/pages/Firma.tsx
+++ src/modules/sign/pages/Firma.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import { useUserStore } from "../../../store/user.store";
 
 function UploadAndDisplayImage() {
     const formData = new FormData();
-    const localItems:any = JSON.parse(localStorage.getItem("user-data") as any);
-    const [userId, setUserId] = useState(localItems["state"]["userInfo"]["numberId"]);
+    const localItems = useUserStore();
+    let userRole = localItems.userInfo.role;
+    const [userId, setUserId] = useState(localItems.userInfo.numberId) as any;
     const img: any = document.querySelector('input[type="file"]')!;
     const [signUrl, setSignUrl] = useState("");
     const [state, setState] = useState(true);
@@ -50,35 +52,43 @@ function UploadAndDisplayImage() {
         formData.append("usuario", userId);
         saveSign(formData);
     }
+    if (userRole == 'SEMILLERISTA' || userRole == 'ESTUDIANTE' || userRole == 'ESTUDIANTE ACTIVO' || userRole == 'ESTUDIANTE INACTIVO') {
+        return (
+            <>
+                <h1>El perfil de su usuario no está autorizado para utilizar esta función</h1>
+            </>
+        )
+    } else {
+        return <>
+            <div>
+                <h1>Firma</h1>
+                <br />
+                <div className="flex-container-center">
+                    <img alt="FIRMA NO REGISTRADA"
+                        width={"250px"}
+                        src={signUrl} />
+                </div>
+                <div className="flex-container-center">
 
-    return <>
-        <div>
-            <h1>Firma</h1>
-            <br />
-            <div className="flex-container-center">
-                <img alt="FIRMA NO REGISTRADA"
-                    width={"250px"}
-                    src={signUrl} />
+                    {state && (
+                        <div>
+                            <input
+                                type="file"
+                                name="myImage"
+                                onChange={(event) => {
+                                    handleImageInput(event);
+                                }}
+                            />
+                            <br />
+                            <br />
+                            <button onClick={handleSaveImage}>Guardar</button>
+                        </div>
+                    )}
+                </div>
             </div>
-            <div className="flex-container-center">
+        </>
+    }
 
-                {state && (
-                    <div>
-                        <input
-                            type="file"
-                            name="myImage"
-                            onChange={(event) => {
-                                handleImageInput(event);
-                            }}
-                        />
-                        <br />
-                        <br/>
-                        <button onClick={handleSaveImage}>Guardar</button>
-                    </div>
-                )}
-            </div>
-        </div>
-    </>
 
 
 }
